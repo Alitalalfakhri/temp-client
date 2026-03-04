2e70bca4e18bd420b696849bd5e947358f908882
Alitalalfakhri <ali.talalfakhri09@gmail.com>
2026-03-05 00:36:05 +0300

    Update API_URL to production environment

--- src/app/lib/lib.js
+++ src/app/lib/lib.js
@@ -1,2 +1,1 @@
-//export const API_URL = 'https://temprory-production.up.railway.app'
-export const API_URL = 'http://localhost:5000'
+export const API_URL = 'https://temprory-production.up.railway.app'
